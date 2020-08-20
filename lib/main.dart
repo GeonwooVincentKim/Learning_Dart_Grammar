d0f94c8f9c93655b7c946b4659cdb93f1b5df01c
kdsnop@gmail.com <kdsnop@gmail.com>
2020-08-20 16:03:05 +0900

    Solving errors.

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,29 @@ void main(){
   *  2. rtl
   *  --> Reads Right-To-Left.
   * */
+
+  // ignore: non_constant_identifier_names
+  body_appearance(){
+    Material(
+      color: Colors.lightBlueAccent,
+      child: Center(
+        child: Column(
+          children: <Widget>[
+            Text(
+              "Hello Flutter",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(color: Colors.white, fontSize: 40.0),
+            ),
+            Text(
+              "Hi Flutter",
+              textDirection: TextDirection.rtl,
+              style: TextStyle(color: Colors.white, fontSize: 40.0),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 //  runApp(
 //    Center(
 //      child: Text(
@@ -71,30 +94,5 @@ void main(){
           body: body_appearance()
         ),
       )
-  );
-
-
-}
-
-// ignore: non_constant_identifier_names
-body_appearance(){
-  Material(
-    color: Colors.lightBlueAccent,
-    child: Center(
-      child: Column(
-        children: <Widget>[
-          Text(
-            "Hello Flutter",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(color: Colors.white, fontSize: 40.0),
-          ),
-          Text(
-            "Hi Flutter",
-            textDirection: TextDirection.rtl,
-            style: TextStyle(color: Colors.white, fontSize: 40.0),
-          ),
-        ],
-      ),
-    ),
   );
 }
