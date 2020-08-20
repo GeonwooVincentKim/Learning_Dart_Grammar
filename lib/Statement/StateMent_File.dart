eb0448f763916690506aa5d225959b85906d2f85
kdsnop@gmail.com <kdsnop@gmail.com>
2020-08-20 20:10:02 +0900

    Defined some variable that haves various-type.

--- lib/Statement/StateMent_File.dart
+++ lib/Statement/StateMent_File.dart
@@ -1,7 +1,12 @@
 import "package:flutter/material.dart";
 
 void main(){
-  String name;
-  name = "홍길동";
-  print(name);
+  String Name;
+  Name = "홍길동";
+
+  int i = 10;
+  double d = 10.0;
+
+  bool b = true;
+  bool b2 = i < 10;
 }
