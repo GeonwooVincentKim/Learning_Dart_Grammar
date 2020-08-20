7590e3eb465276c1d7851bb67da7251e413afdc8
kdsnop@gmail.com <kdsnop@gmail.com>
2020-08-20 16:17:49 +0900

    Commented unnecessary files in 'main.dart'.

--- lib/main.dart
+++ lib/main.dart
@@ -95,24 +95,24 @@ void main(){
 }
 
 // ignore: non_constant_identifier_names
-body_appearance() {
-  Material(
-    color: Colors.lightBlueAccent,
-    child: Center(
-      child: Column(
-        children: <Widget>[
-          Text(
-            "Hello Flutter",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(color: Colors.white, fontSize: 40.0),
-          ),
-          Text(
-            "Hi Flutter",
-            textDirection: TextDirection.rtl,
-            style: TextStyle(color: Colors.white, fontSize: 40.0),
-          ),
-        ],
-      ),
-    ),
-  );
-}
+//body_appearance() {
+//  Material(
+//    color: Colors.lightBlueAccent,
+//    child: Center(
+//      child: Column(
+//        children: <Widget>[
+//          Text(
+//            "Hello Flutter",
+//            textDirection: TextDirection.ltr,
+//            style: TextStyle(color: Colors.white, fontSize: 40.0),
+//          ),
+//          Text(
+//            "Hi Flutter",
+//            textDirection: TextDirection.rtl,
+//            style: TextStyle(color: Colors.white, fontSize: 40.0),
+//          ),
+//        ],
+//      ),
+//    ),
+//  );
+//}
