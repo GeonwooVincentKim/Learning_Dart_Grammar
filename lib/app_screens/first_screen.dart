5201375581ad79ef51f40f8e46b6df51a447faf0
kdsnop@gmail.com <kdsnop@gmail.com>
2020-08-21 20:36:50 +0900

    Modifying FirstScreen Class.

--- lib/app_screens/first_screen.dart
+++ lib/app_screens/first_screen.dart
@@ -3,7 +3,24 @@ import 'package:flutter/material.dart';
 class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Material(
+      color: Colors.lightBlueAccent,
+      child: Center(
+        child: Column(
+          children: <Widget>[
+            Text(
+              "Hello Flutter",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(color: Colors.white, fontSize: 40.0),
+            ),
+            Text(
+              "Hi Flutter",
+              textDirection: TextDirection.rtl,
+              style: TextStyle(color: Colors.white, fontSize: 40.0),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
