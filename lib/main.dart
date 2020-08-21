0fbc814a4b76be3b5520001e02cfe168d9c52e14
kdsnop@gmail.com <kdsnop@gmail.com>
2020-08-21 17:39:27 +0900

    Moved old-codes to subsitute_code.dart from main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -28,29 +28,6 @@ void main(){
   );
 }
 
-// ignore: non_constant_identifier_names
-//body_appearance() {
-//  return Material(
-//    color: Colors.lightBlueAccent,
-//    child: Center(
-//      child: Column(
-//        children: <Widget>[
-//          Text(
-//            "Hello Flutter",
-//            textDirection: TextDirection.ltr,
-//            style: TextStyle(color: Colors.white, fontSize: 40.0),
-//          ),
-//          Text(
-//            "Hi Flutter",
-//            textDirection: TextDirection.rtl,
-//            style: TextStyle(color: Colors.white, fontSize: 40.0),
-//          ),
-//        ],
-//      ),
-//    ),
-//  );
-//}
-
 
 /*
 * 1. StatelessWidget --> Super-Class.
--- lib/subsitute_code.dart
+++ lib/subsitute_code.dart
@@ -21,24 +21,49 @@ void subsitute_code(){
 //        )
 //    );
 
-    return runApp(
-      new MaterialApp(
-        title: "My Flutter Application",
-        home: new Material(
-          color: Colors.lightBlueAccent,
-          child: Center(
-            child: Text(
-              "Hello Flutter",
-              textDirection: TextDirection.ltr,
-              style: TextStyle(color: Colors.white, fontSize: 40.0),
-            ),
-          ),
-        ),
-      )
-    );
+//    return runApp(
+//      new MaterialApp(
+//        title: "My Flutter Application",
+//        home: new Material(
+//          color: Colors.lightBlueAccent,
+//          child: Center(
+//            child: Text(
+//              "Hello Flutter",
+//              textDirection: TextDirection.ltr,
+//              style: TextStyle(color: Colors.white, fontSize: 40.0),
+//            ),
+//          ),
+//        ),
+//      )
+//    );
+
+
 }
 
 // ignore: non_constant_identifier_names
 test_print(){
    return print("Hello World");
+}
+
+// ignore: non_constant_identifier_names
+body_appearance() {
+  return Material(
+    color: Colors.lightBlueAccent,
+    child: Center(
+      child: Column(
+        children: <Widget>[
+          Text(
+            "Hello Flutter",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(color: Colors.white, fontSize: 40.0),
+          ),
+          Text(
+            "Hi Flutter",
+            textDirection: TextDirection.rtl,
+            style: TextStyle(color: Colors.white, fontSize: 40.0),
+          ),
+        ],
+      ),
+    ),
+  );
 }
