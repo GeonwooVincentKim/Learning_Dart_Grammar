90ef5250ea7d1e4c1e67c7e9a8e72d005e92b474
kdsnop@gmail.com <kdsnop@gmail.com>
2020-08-21 16:50:55 +0900

    Copy codes from original function named 'body_appearance'.

--- lib/main.dart
+++ lib/main.dart
@@ -85,7 +85,25 @@ class body_appearance extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    throw UnimplementedError();
+    return Material(
+      color: Colors.lightBlueAccent,
+      child: Center(
+        child: Column(
+          children: <Widget>[
+            Text(
+              "Hello Flutter",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(color: Colors.white, fontSize: 40.0),
+            ),
+            Text(
+              "Hi Flutter",
+              textDirection: TextDirection.rtl,
+              style: TextStyle(color: Colors.white, fontSize: 40.0),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
 }
