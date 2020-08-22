b5cfb37b737cbd18c7f1e69c620b85bb3d2f135d
kdsnop@gmail.com <kdsnop@gmail.com>
2020-08-22 20:47:00 +0900

    Additional attributes of 'Container' forms.

--- lib/second_section/app_screens/Home.dart
+++ lib/second_section/app_screens/Home.dart
@@ -4,6 +4,13 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Container();
+    // A Containers first surrounds the child with padding
+    // and then applies additional constraints to the padded
+    // extent (width and height).
+    return Container(
+      alignment: Alignment.center,
+      color: Colors.deepPurple,
+      child: Text("Flight", textDirection: TextDirection.ltr),
+    );
   }
 }
