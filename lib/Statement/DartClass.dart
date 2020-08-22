ed94bac70a984ce54fe8bc0f539384d6ddb617a9
kdsnop@gmail.com <kdsnop@gmail.com>
2020-08-22 17:42:37 +0900

    Selective-Function.

--- lib/Statement/DartClass.dart
+++ lib/Statement/DartClass.dart
@@ -14,16 +14,21 @@ class Person{
   int age;
   Person(this.name, {this.age});
   void greeting() => print("안녕하세요, 저는 $name입니다");
+
+
 }
 
-
-(number) {
-  return number %2 == 0;
-};
+person (number) => number % 2 == 0;
+void something({String name, int age}) {}
 
 void main(){
   // var myClass = MyClass();
   print(MyClass.isEven(10));
   Person person = Person("kkw", age: 19);
   person.greeting();
+
+  something(name: "홍길동", age: 10);
+  something(name: "홍길동");
+  something(age: 10);
+  something();
 }
