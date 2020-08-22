0c7db4bb5e95dbd1437058cd9b5071b6096e88f4
kdsnop@gmail.com <kdsnop@gmail.com>
2020-08-22 19:51:28 +0900

    Added 'if', 'else if', 'ellse' phrase.:

--- lib/Statement/if_else.dart
+++ lib/Statement/if_else.dart
@@ -2,3 +2,13 @@ import 'package:flutter/material.dart';
 
 
 String text = 'hello';
+
+void main(){
+  if (text is int){
+    print("Integer");
+  } else if (text is double){
+    print("Real-Number");
+  } else {
+    print("Not both of them");
+  }
+}
