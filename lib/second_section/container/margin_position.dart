3ab927fba10ee097382a48ecbfdaa03c2250643f
kdsnop@gmail.com <kdsnop@gmail.com>
2020-08-23 19:33:45 +0900

    Created margin_wh function.

--- lib/second_section/container/margin_position.dart
+++ lib/second_section/container/margin_position.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+
+
+// Convert the size by using width and height.
+// ignore: non_constant_identifier_names
+margin_wh(){
+  return Center(
+    child: Container(
+      alignment: Alignment.center,
+      color: Colors.deepPurple,
+      // It doesn't work when you only write width-size and height-size.
+      // (Only display Container only)
+
+      width: 200.0,
+      height: 100.0,
+      child: Text("Flight", textDirection: TextDirection.ltr),
+    )
+  );
+}
