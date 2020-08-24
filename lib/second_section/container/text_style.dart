9a705863302ce6c1751899ac1b89b81ee8c00e0a
kdsnop@gmail.com <kdsnop@gmail.com>
2020-08-24 09:46:52 +0900

    Created text_font_size

--- lib/second_section/container/text_style.dart
+++ lib/second_section/container/text_style.dart
@@ -17,3 +17,22 @@ text_decoration(){
       )
   );
 }
+
+
+// ignore: non_constant_identifier_names
+text_font_size(){
+  return Center(
+      child: Container(
+        alignment: Alignment.center,
+        color: Colors.deepPurple,
+        child: Text(
+          "Flight",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            decoration: TextDecoration.none,
+            fontSize: 75.0
+          ),
+        ),
+      )
+  );
+}
