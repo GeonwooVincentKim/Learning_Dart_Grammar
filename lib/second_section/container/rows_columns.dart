7d91e5dea0d7906eee28b8d0a52250b107ec23b4
kdsnop@gmail.com <kdsnop@gmail.com>
2020-08-24 14:57:15 +0900

    Copied codes.

--- lib/second_section/container/rows_columns.dart
+++ lib/second_section/container/rows_columns.dart
@@ -1,3 +1,82 @@
 import 'package:flutter/material.dart';
 
 
+// ignore: non_constant_identifier_names
+rows_non_expanded(){
+  return Center(
+      child: Container(
+          alignment: Alignment.center,
+          color: Colors.deepPurple,
+          child: Row(
+            children: <Widget>[
+              Text(
+                "Korean-Air",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 35.0,
+                    fontFamily: "Raleway",
+                    fontWeight: FontWeight.w700,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.white
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  "From Seoul/Incheon to Oslo via Paris.",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 35.0,
+                      fontFamily: "Raleway",
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.white
+                  ),
+                ),
+              ),
+            ],
+          )
+      )
+  );
+}
+
+
+rows_expanded(){
+  return Center(
+      child: Container(
+          alignment: Alignment.center,
+          color: Colors.deepPurple,
+          child: Row(
+            children: <Widget>[
+              Text(
+                "Korean-Air",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 35.0,
+                    fontFamily: "Raleway",
+                    fontWeight: FontWeight.w700,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.white
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  "From Seoul/Incheon to Oslo via Paris.",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 35.0,
+                      fontFamily: "Raleway",
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.white
+                  ),
+                ),
+              ),
+            ],
+          )
+      )
+  );
+}
