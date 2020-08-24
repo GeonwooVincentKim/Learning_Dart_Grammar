395245d528bf5bef7c36a5df29556ed0e3703d38
kdsnop@gmail.com <kdsnop@gmail.com>
2020-08-24 14:41:41 +0900

    Moved unnecessary comments to 'container_position.dart' from 'Home.dart'.

--- lib/second_section/app_screens/Home.dart
+++ lib/second_section/app_screens/Home.dart
@@ -7,17 +7,6 @@ import 'package:flutter_dart_grammer/second_section/container/text_style.dart';
 class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // A Containers first surrounds the child with padding
-    // and then applies additional constraints to the padded
-    // extent (width and height).
-
-    // Import 'container_position.dart' file and
-    // apply to return phrase.
-    // return center_container();
-
-    // Only display in center-part.
-    // The Center-Attribute that only display Center part
-    // currently included Container-Attribute.
     return Center(
       child: Container(
         alignment: Alignment.center,
--- lib/second_section/container/container_position.dart
+++ lib/second_section/container/container_position.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 
 
+// A Containers first surrounds the child with padding
+// and then applies additional constraints to the padded
+// extent (width and height).
+
+// Import 'container_position.dart' file and
+// apply to return phrase.
+// return center_container();
+
+// Only display in center-part.
+// The Center-Attribute that only display Center part
+// currently included Container-Attribute.
 // ignore: non_constant_identifier_names
 center_container(){
   return Container(
