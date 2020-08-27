d78d9cf150717442e8d000ee76a4165afd8e5762
kdsnop@gmail.com <kdsnop@gmail.com>
2020-08-27 18:18:31 +0900

    Created 'main' function.

--- lib/second_one_section/main.dart
+++ lib/second_one_section/main.dart
@@ -1,3 +1,11 @@
 import 'package:flutter/material.dart';
 import './app_screens/home.dart';
 
+void main(){
+  runApp(
+    MaterialApp(
+      title: "Exploring UI widgets",
+      home: Home()
+    ),
+  );
+}
