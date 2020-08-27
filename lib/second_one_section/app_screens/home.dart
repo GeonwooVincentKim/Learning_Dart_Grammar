3e859994d266cb873a51766c22e7cdd8f635aeeb
kdsnop@gmail.com <kdsnop@gmail.com>
2020-08-27 18:17:30 +0900

    Added codes that already exist in 'second_section\app_screens'.

---
+++ lib/second_one_section/app_screens/home.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dart_grammer/second_section/container/container_position.dart';
+import 'package:flutter_dart_grammer/second_section/container/margin_position.dart';
+import 'package:flutter_dart_grammer/second_section/container/text_style.dart';
+
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+            padding: EdgeInsets.only(left: 10.0, top: 40.0),
+            alignment: Alignment.center,
+            color: Colors.deepPurple,
+            /*
+         * Column makes UI Position as the
+         * upper half of the Layout.
+         * */
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        "Korean-Air",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 30.0,
+                            fontFamily: "Raleway",
+                            fontWeight: FontWeight.w700,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        "From Seoul/Incheon to Oslo via Paris.",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 22.5,
+                            fontFamily: "Raleway",
+                            fontWeight: FontWeight.w700,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        "Korean-Air",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 30.0,
+                            fontFamily: "Raleway",
+                            fontWeight: FontWeight.w700,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        "From Seoul/Incheon to Oslo via Paris.",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 22.5,
+                            fontFamily: "Raleway",
+                            fontWeight: FontWeight.w700,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            )
+        )
+    );
+  }
+}
+
+
