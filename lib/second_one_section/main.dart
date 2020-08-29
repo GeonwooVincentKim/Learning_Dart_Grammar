c12372a8e05023a5d508e6ca856b209bb8a75274
kdsnop@gmail.com <kdsnop@gmail.com>
2020-08-29 20:56:59 +0900

    Generated getListElements function.

--- lib/second_one_section/main.dart
+++ lib/second_one_section/main.dart
@@ -6,10 +6,22 @@ void main(){
     MaterialApp(
       title: "Exploring UI widgets",
       home: Scaffold(
-        appBar: AppBar(title: Text("Basic List View"),),
-        body: Home(),
+        // appBar: AppBar(title: Text("Basic List View"),),
+        appBar: AppBar(title: Text("Long List"),),
       ),
     ),
   );
 }
 
+// Generate List-Item from 0 to 999.
+// ignore: missing_return
+List<String> getListElements(){
+  var items = List<String>.generate(1000, (counter)=>"Item $counter");
+  return items;
+}
+
+
+// ignore: missing_return
+Widget getListView(){
+
+}
