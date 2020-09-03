27b4b35982879e453041289993ae9b34bc259921
kdsnop@gmail.com <kdsnop@gmail.com>
2020-09-03 20:09:03 +0900

    Generated 'Home' Class in 'Home.dart' file.

--- lib/third_section/app_screens/Home.dart
+++ lib/third_section/app_screens/Home.dart
@@ -6,4 +6,15 @@ import 'package:flutter/material.dart';
 * - The state is information that can read synchronously
 *   when the widget is build and might change during the
 *   lifetime of the widget.
+*
+* - Classes that inherit "StatefulWidget" are immutable.
+* - But, the 'State' is mutable.
 * */
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
--- lib/third_section/main.dart
+++ lib/third_section/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dart_grammer/third_section/app_screens/Home.dart';
 
 
 void main(){
   runApp(
     MaterialApp(
-
+      title: "Third_Section",
+      home: Home(),
     ),
   );
 }
