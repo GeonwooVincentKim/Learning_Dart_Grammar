027b3c6b5646b70bbc1c74ada12899dd85da3e00
kdsnop@gmail.com <kdsnop@gmail.com>
2020-09-03 21:06:12 +0900

    Added new comments.

--- lib/third_section/app_screens/FavoriteCity.dart
+++ lib/third_section/app_screens/FavoriteCity.dart
@@ -21,9 +21,19 @@ class _FavoriteCityClass extends State<FavoriteCity>{
   Widget build(BuildContext context) {
     return Column(
       children: <Widget> [
+        /*
+        * - 1. Added a 'TextField' attribute to allow user
+        * to enter some Text.
+        *
+        * - 2. Such as 'FavoriteCity'.
+        * */
         TextField(
           onSubmitted: (String string) {
-
+            // Define the handle-matter where we assigning
+            // the variable name, 'String name = "";'
+            // which is our property equal to the
+            // 'UserInput'.
+            setState(() { name=string;});
           },
         ),
       ],
