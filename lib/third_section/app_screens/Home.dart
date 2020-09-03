60c7cb7f5d64e9e436daa410c80fd3fc2e2c243f
kdsnop@gmail.com <kdsnop@gmail.com>
2020-09-03 20:15:59 +0900

    Added comments that related to Stateful-Widget and Stateless-Widget'.

--- lib/third_section/app_screens/Home.dart
+++ lib/third_section/app_screens/Home.dart
@@ -9,7 +9,18 @@ import 'package:flutter/material.dart';
 *
 * - Classes that inherit "StatefulWidget" are immutable.
 * - But, the 'State' is mutable.
+*
+* 2. Stateful Widget
+* - When a widget changes (user interacts with it),
+*   it's 'Stateful'.
+* - Such as 'CheckBox', 'RadioButton', 'Form', 'TextField'.
+*
+* 3. Stateless Widget
+* - No internal state to manage or no direct user interaction,
+*   it's 'Stateless'.
+* - Such as 'Text', 'RaisedButton', 'Icon', 'IconButton'.
 * */
+
 
 class Home extends StatelessWidget{
   @override
