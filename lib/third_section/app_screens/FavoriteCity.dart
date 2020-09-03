37d5db83cd4c5ff3d9486ce103e24985ebfb6b33
kdsnop@gmail.com <kdsnop@gmail.com>
2020-09-03 20:53:45 +0900

    Generated 'Favorite.dart' and created inheritated class named '_FavoriteCityClass'.

---
+++ lib/third_section/app_screens/FavoriteCity.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+class FavoriteCity extends StatefulWidget {
+  @override
+  // ignore: missing_return
+  State<StatefulWidget> createState(){
+
+  }
+}
+
+/*
+* - 1. By Using super class of State, as 'FavoriteCity'.
+* - 2. The reason why we use 'State<FavoriteCity>' is
+*      Defining 'State<FavoriteCity>'.
+* */
+class _FavoriteCityClass extends State<FavoriteCity>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
