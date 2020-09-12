824abdc4f6d491b7b1bb85f0c513351d53aacc78
kdsnop@gmail.com <kdsnop@gmail.com>
2020-09-12 21:52:11 +0900

    Generated 'third_one_section'.

---
+++ lib/third_one_section/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dart_grammer/third_one_section/app_screens/Home.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: "Title",
+    home: Home(),
+  ));
+}
