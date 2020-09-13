4de5cba20126e5020d90988165e1194a488815a1
kdsnop@gmail.com <kdsnop@gmail.com>
2020-09-13 20:31:43 +0900

    Generate and re-manufacturing forms of 'main.dart' file.

--- lib/third_one_section/main.dart
+++ lib/third_one_section/main.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dart_grammer/second_one_section/container/list_tile.dart';
 import 'package:flutter_dart_grammer/third_one_section/app_screens/Home.dart';
 
 void main(){
   runApp(MaterialApp(
-    title: "Title",
-    home: Home(),
+    title: "Exploring UI widgets",
+    home: Scaffold(
+    appBar: AppBar(title: Text("Long List"),),
+      body: getListView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          debugPrint("FAB Clicked");
+        },
+        child: Icon(Icons.add),
+        tooltip: 'Add one More Item',
+      ),
+    ),
   ));
+}
+
+void showSnackBar(BuildContext context, String item){
+  var snackBar = SnackBar(
+    content: Text("You just tapped $item"),
+    action: SnackBarAction(
+      label: "UNDO",
+      onPressed: (){
+        debugPrint("Performing dummy UNDO operation");
+      },
+    ),
+  );
+
+  Scaffold.of(context).showSnackBar(snackBar);
 }
